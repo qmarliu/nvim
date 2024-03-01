363cb0d97e6e0b14d96bae414a73c4c3f35e9835
liulei <liu6uu@163.com>
2024-03-01 20:13:32 +0700

    退出vim，esc等快捷键配置;设置neo-tree位置;nvim-notify的通知位置

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- use `vim.keymap.set` instead
+local map = vim.api.nvim_set_keymap
+
+local opt = {
+  noremap = true,
+  silent = true,
+}
+
+map("v", "q", "<ESC>", opt)
+map("!", "<C-q>", "<ESC>", { noremap = true })
+
+map("n", "<C-q>", ":qa<CR>", opt)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+
+-- 右侧参考线，超过表示代码太长了，考虑换行
+vim.wo.colorcolumn = "120"
